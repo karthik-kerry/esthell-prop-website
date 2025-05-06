a0918b647760a4476a65b8ab857c0b8952e9b225
Sujitha-ki <sujitha.sakthivel@timelapseinformatics.com>
2025-05-06 14:26:33 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import SearchPage from "./pages/SearchPage";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="details" element={<DetailsPage />} />
+        <Route path="search" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
   );
