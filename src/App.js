2f3c14717238a0edd6b0f603c22b7d2eb1d4b537
karthik-kerry <karthikeyan.ns@timelapseinformatics.com>
2025-05-07 17:03:49 +0530

    listing details page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import ListingsPage from "./pages/ListingsPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
+import ListingDetailsPage from "./pages/ListingDetailsPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="listings" element={<ListingsPage />} />
+        <Route path="listing-Detail" element={<ListingDetailsPage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="contact" element={<ContactPage />} />
       </Routes>
---
+++ src/pages/ListingDetailsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function ListingDetailsPage() {
+  return (
+    <div>
+      <Header />
+      <div style={{ height: 100 }}>Listing Details Page</div>
+      <Footer />
+    </div>
+  );
+}
